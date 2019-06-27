e65daafb25e2ec03636089c715172be196fdfbb4
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-06-27 15:59:16 +0200

    First Test fix

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
@@ -1,31 +1,44 @@
 package edu.iis.mto.testreactor.exc4;
 
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.junit.Before;
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+
 
 
 public class DishWasherTest {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    private Door door;
+
+    private DirtFilter dirtFilter;
+
+    private Engine engine;
+
+    private WaterPump waterPump;
+
+    @Before
+    public void setUp() {
+        door = mock(Door.class);
+        dirtFilter = mock(DirtFilter.class);
+        engine = mock(Engine.class);
+        waterPump = mock(WaterPump.class);
     }
+
+
     @Test
     public void dishWasherStartShouldReturnDoorOpenErrorIfDoorIsOpen() {
-        Door door = mock(Door.class);
-        DirtFilter dirtFilter = mock(DirtFilter.class);
-        Engine engine = mock(Engine.class);
-        WaterPump waterpPump = mock(WaterPump.class);
-
         when(door.closed()).thenReturn(true);
         when(dirtFilter.capacity()).thenReturn(60.0d);
 
-        DishWasher dishWasher = new DishWasher(waterpPump, engine, dirtFilter, door);
+        DishWasher dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
         ProgramConfiguration program = ProgramConfiguration.builder().withProgram(WashingProgram.ECO).withTabletsUsed(true).build();
         RunResult result = dishWasher.start(program);
         RunResult expectedResult = RunResult.builder().withStatus(Status.DOOR_OPEN).build();
-        assertThat(result, is(expectedResult));
+        assertThat(result.getStatus(), is(expectedResult.getStatus()));
     }
-    
+
+
 }
